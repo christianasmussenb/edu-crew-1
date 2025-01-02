0b7ab210532a02d7561e77ea19613340236e2b1e
CHRISTIAN ASMUSSEN <christian@casmuss.com>
2025-01-02 19:13:47 -0300

    update LLM configurations and refine task descriptions for clarity and detail

--- edu_flow2/src/edu_flow2/config.py
+++ edu_flow2/src/edu_flow2/config.py
@@ -2,11 +2,11 @@ import os
 
 LLM_CONFIGS = {
     "openai": {
-        "model": "gpt-4o-mini",
+        "model": "gpt-4o-mini", # "gpt-4o-mini" or "gpt-4o"
         "api_key": os.getenv('OPENAI_API_KEY')
     },
     "groq": {
-        "model": "groq/llama3-groq-70b-8192-tool-use-preview", 
+        "model": "groq/llama3-groq-8b-8192-tool-use-preview", 
         "api_key": os.getenv('GROQ_API_KEY')
     },
     "anthropic": {
@@ -19,5 +19,5 @@ LLM_CONFIG = LLM_CONFIGS["openai"] # Change this to switch between LLMs
 
 EDU_FLOW_INPUT_VARIABLES = {
     "audience_level": "intermediate",
-    "topic": "Automated reasoning"
+    "topic": "Healthcare and AI, most recent advancements",
 } 
